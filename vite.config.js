f675e7becd76037dd6b9a01a554f88bf806cf86c
lisenapati <lisenhatson@gmail.com>
2025-04-29 22:32:23 +0700

    upload

---
+++ vite.config.js
@@ -0,0 +1,18 @@
+import { fileURLToPath, URL } from 'node:url'
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import path from 'path'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      process: path.resolve(__dirname, 'node_modules/process/browser'),
+    },
+  },
+})
+
