5a43188bf68e266d9d310e2e0230a75329b7ce2e
lisenapati <lisenhatson@gmail.com>
2025-04-29 22:56:41 +0700

    gitaction

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
-  base: '/notnetflix/',
+  base: '/pbl-notnetflix/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
